a4caf68f30ada30f487f4752e68694523b571d8a
Ricky Kearney <14141647@studentmail.ul.ie>
2018-02-19 16:07:34 +0000

    Added RSA key swaps and step 1 of protocol

--- app.js
+++ app.js
@@ -3,43 +3,69 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
 var chatEncrypted = false;
+var userCount = 0;
 
 app.get('/', function(req, res) {
-  res.sendfile('index.html');
+ res.sendfile('index.html');
 });
 
 app.use(express.static(__dirname + '/public'));
 
 http.listen(3000, "0.0.0.0", function() {
-  console.log('listening on *:3000');
+ console.log('listening on *:3000');
 });
 
 io.on('connection', function(socket) {
+ userCount += 1;
+ if (userCount == 2) {
+  io.emit('startRSA');
+ }
 
-  // send normal message to everyone
-  socket.on('normalMessage', function(msg) {
-    io.emit('normalMessage', msg);
-  });
+ // send normal message to everyone
+ socket.on('normalMessage', function(msg) {
+  io.emit('normalMessage', msg);
+ });
 
-  // send encrypted message to everyone
-  socket.on('encryptedMessage', function(msg) {
-    if (chatEncrypted) {
-      io.emit('encryptedMessage', msg);
-    } else {
-      console.log("Chat not encrypted yet");
-    }
-  });
+ // send encrypted message to everyone
+ socket.on('encryptedMessage', function(msg) {
+  if (chatEncrypted) {
+   io.emit('encryptedMessage', msg);
+  } else {
+   console.log("Chat not encrypted yet");
+  }
+ });
 
-  // User started the encrypt protocol
-  socket.on('encryptProtocol', function(msg) {
-    console.log("protocol started");
-    socket.broadcast.emit('getResponse1', 'Encryption protocol Started by other client');
-    socket.broadcast.emit('encryptedMessage', 'Encryption protocol Started by other client');
-    chatEncrypted = true; // when we have a sucessful encrypted pipe
-  });
+ // User started the encrypt protocol
+ socket.on('encryptProtocol', function(msg) {
+  console.log("Encryption protocol started");
+  socket.broadcast.emit('encryptedMessage', 'Encryption protocol Started by other client');
+  socket.broadcast.emit('step1'); // Start step 1
+  chatEncrypted = true; // when we have a sucessful encrypted pipe
+ });
+
+ // swap RSA public keys
+ socket.on('sendPublicKey', function(msg) {
+  socket.broadcast.emit('receivePublicKey', msg);
+ });
+
+ // Step 2
+ socket.on('getResponseStep2', function(msg) {
+  socket.broadcast.emit('step2', msg);
+ });
+
+ // Step 3
+ socket.on('getResponseStep3', function(msg) {
+  socket.broadcast.emit('step3', msg);
+ });
+
+ // Step 4
+ socket.on('getResponseStep4', function(msg) {
+  socket.broadcast.emit('step4', msg);
+ });
 
 
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-  });
+ socket.on('disconnect', function() {
+  //console.log('user disconnected');
+  userCount += -1;
+ });
 });
--- public/js/EncrypChat.js
+++ public/js/EncrypChat.js
@@ -1,165 +1,278 @@
+// Hash with SHA
+function hashSHA(data) {
+ return window.crypto.subtle.digest({
+    name: "SHA-256",
+   },
+   data //The data you want to hash as an ArrayBuffer
+  )
+  .then(function(hash) {
+   //returns the hash as an ArrayBuffer
+   var hashed = new Uint8Array(hash);
+   // console.log(hashed);
+   return hashed;
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
+}
+
 // Generate The RSA keypair
 function generateRSA() {
-  return window.crypto.subtle.generateKey({
-        name: "RSA-PSS",
-        modulusLength: 2048, //can be 1024, 2048, or 4096
-        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
-        hash: {
-          name: "SHA-256"
-        }, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
-      },
-      false, //whether the key is extractable (i.e. can be used in exportKey)
-      ["sign", "verify"] //can be any combination of "sign" and "verify"
-    )
-    .then(function(key) {
-      //returns a keypair object
-      console.log(key);
-      console.log(key.publicKey);
-      console.log(key.privateKey);
-      return key;
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
+ return window.crypto.subtle.generateKey({
+    name: "RSA-PSS",
+    modulusLength: 2048, //can be 1024, 2048, or 4096
+    publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+    hash: {
+     name: "SHA-256"
+    }, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+   },
+   true, //whether the key is extractable (i.e. can be used in exportKey)
+   ["sign", "verify"] //can be any combination of "sign" and "verify"
+  )
+  .then(function(key) {
+   //returns a keypair object
+   // console.log(key);
+   // console.log(key.publicKey);
+   // console.log(key.privateKey);
+   return key;
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
+}
+
+function exportRSA(publicKey) {
+ return window.crypto.subtle.exportKey(
+   "spki", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+   publicKey //can be a publicKey or privateKey, as long as extractable was true
+  )
+  .then(function(keydata) {
+   //returns the exported key data
+   return keydata;
+   // console.log(keydata);
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
+}
+
+function importRSA_PSS(exportedKey) {
+ return window.crypto.subtle.importKey(
+   "spki", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+   exportedKey, { //these are the algorithm options
+    name: "RSA-PSS",
+    hash: {
+     name: "SHA-256"
+    }, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+   },
+   false, //whether the key is extractable (i.e. can be used in exportKey)
+   ["verify"] //"verify" for public key import, "sign" for private key imports
+  )
+  .then(function(publicKey) {
+   //returns a publicKey (or privateKey if you are importing a private key)
+   // console.log(publicKey);
+   return publicKey;
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
+}
+
+function importRSA_OAEP(exportedKey) {
+ return window.crypto.subtle.importKey(
+   "spki", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+   exportedKey, { //these are the algorithm options
+    name: "RSA-OAEP",
+    hash: {
+     name: "SHA-256"
+    }, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+   },
+   false, //whether the key is extractable (i.e. can be used in exportKey)
+   ["encrypt"] //"encrypt" or "wrapKey" for public key import or
+   //"decrypt" or "unwrapKey" for private key imports
+  )
+  .then(function(publicKey) {
+   //returns a publicKey (or privateKey if you are importing a private key)
+   // console.log(publicKey);
+   return publicKey;
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
 }
 
 // RSA Sign
-function signRSA() {
-  return window.crypto.subtle.sign({
-        name: "RSA-PSS",
-        saltLength: 128, //the length of the salt
-      },
-      privateKey, //from generateKey or importKey above
-      data //ArrayBuffer of data you want to sign
-    )
-    .then(function(signature) {
-      //returns an ArrayBuffer containing the signature
-      console.log(new Uint8Array(signature));
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
+function signRSA(privateKey, data) {
+ return window.crypto.subtle.sign({
+    name: "RSA-PSS",
+    saltLength: 128, //the length of the salt
+   },
+   privateKey, //from generateKey or importKey above
+   data //ArrayBuffer of data you want to sign
+  )
+  .then(function(signature) {
+   //returns an ArrayBuffer containing the signature
+   var data = new Uint8Array(signature);
+   //console.log();
+   return data;
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
+}
+
+// encrypt with publicKey
+function encryptRSA(publicKey, data) {
+ return window.crypto.subtle.encrypt({
+    name: "RSA-OAEP",
+    //label: Uint8Array([...]) //optional
+   },
+   publicKey, //from generateKey or importKey above
+   new Uint8Array(data) //ArrayBuffer of data you want to encrypt
+  )
+  .then(function(encrypted) {
+   //returns an ArrayBuffer containing the encrypted data
+   var data = new Uint8Array(encrypted);
+   return data;
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
+}
+
+// decrypt with publicKey
+function decryptRSA(publicKey, data) {
+ return window.crypto.subtle.decrypt({
+    name: "RSA-OAEP",
+    //label: Uint8Array([...]) //optional
+   },
+   privateKey, //from generateKey or importKey above
+   new Uint8Array(data) //ArrayBuffer of the data
+  )
+  .then(function(decrypted) {
+   //returns an ArrayBuffer containing the decrypted data
+   var data = new Uint8Array(decrypted);
+   return data;
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
 }
 
 // RSA verify
 function verifyRSA() {
-  return window.crypto.subtle.verify({
-        name: "RSA-PSS",
-        saltLength: 128, //the length of the salt
-      },
-      publicKey, //from generateKey or importKey above
-      signature, //ArrayBuffer of the signature
-      data //ArrayBuffer of the data
-    )
-    .then(function(isvalid) {
-      //returns a boolean on whether the signature is true or not
-      console.log(isvalid);
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
+ return window.crypto.subtle.verify({
+    name: "RSA-PSS",
+    saltLength: 128, //the length of the salt
+   },
+   publicKey, //from generateKey or importKey above
+   signature, //ArrayBuffer of the signature
+   data //ArrayBuffer of the data
+  )
+  .then(function(isvalid) {
+   //returns a boolean on whether the signature is true or not
+   console.log(isvalid);
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
 }
 
 // Generate AES Key
 function generateAES() {
-  return window.crypto.subtle.generateKey({
-        name: "AES-GCM",
-        length: 256, //can be  128, 192, or 256
-      },
-      true, //whether the key is extractable (i.e. can be used in exportKey)
-      ["encrypt", "decrypt"] //can "encrypt", "decrypt", "wrapKey", or "unwrapKey"
-    )
-    .then(function(key) {
-      //returns a key object
-      //console.log(key);
-      return key;
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
+ return window.crypto.subtle.generateKey({
+    name: "AES-GCM",
+    length: 256, //can be  128, 192, or 256
+   },
+   true, //whether the key is extractable (i.e. can be used in exportKey)
+   ["encrypt", "decrypt"] //can "encrypt", "decrypt", "wrapKey", or "unwrapKey"
+  )
+  .then(function(key) {
+   //returns a key object
+   //console.log(key);
+   return key;
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
 }
 
 function AESEncrypt(key, data) {
-  var generatedIv = window.crypto.getRandomValues(new Uint8Array(12))
-  return window.crypto.subtle.encrypt({
-        name: "AES-GCM",
+ var generatedIv = window.crypto.getRandomValues(new Uint8Array(12));
+ return window.crypto.subtle.encrypt({
+    name: "AES-GCM",
 
-        //Don't re-use initialization vectors!
-        //Always generate a new iv every time your encrypt!
-        //Recommended to use 12 bytes length
-        iv: generatedIv,
+    //Don't re-use initialization vectors!
+    //Always generate a new iv every time your encrypt!
+    //Recommended to use 12 bytes length
+    iv: generatedIv,
 
-        //Additional authentication data (optional)
-        // additionalData: ArrayBuffer,
+    //Additional authentication data (optional)
+    // additionalData: ArrayBuffer,
 
-        //Tag length (optional)
-        tagLength: 128, //can be 32, 64, 96, 104, 112, 120 or 128 (default)
-      },
-      key, //from generateKey or importKey above
-      data //ArrayBuffer of data you want to encrypt
-    )
-    .then(function(encrypted) {
-      //returns an ArrayBuffer containing the encrypted data
-      var data = new Uint8Array(encrypted);
-      return [key, data, generatedIv];
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
+    //Tag length (optional)
+    tagLength: 128, //can be 32, 64, 96, 104, 112, 120 or 128 (default)
+   },
+   key, //from generateKey or importKey above
+   data //ArrayBuffer of data you want to encrypt
+  )
+  .then(function(encrypted) {
+   //returns an ArrayBuffer containing the encrypted data
+   var data = new Uint8Array(encrypted);
+   return [key, data, generatedIv];
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
 }
 
 function AESDecrypt(key, data, generatedIv) {
-  return window.crypto.subtle.decrypt({
-        name: "AES-GCM",
-        iv: generatedIv, //The initialization vector you used to encrypt
-        //additionalData: ArrayBuffer, //The addtionalData you used to encrypt (if any)
-        tagLength: 128, //The tagLength you used to encrypt (if any)
-      },
-      key, //from generateKey or importKey above
-      data //ArrayBuffer of the data
-    )
-    .then(function(decrypted) {
-      //returns an ArrayBuffer containing the decrypted data
-      var data = new Uint8Array(decrypted);
-      return data
-    })
-    .catch(function(err) {
-      console.error(err);
-    });
+ return window.crypto.subtle.decrypt({
+    name: "AES-GCM",
+    iv: generatedIv, //The initialization vector you used to encrypt
+    //additionalData: ArrayBuffer, //The addtionalData you used to encrypt (if any)
+    tagLength: 128, //The tagLength you used to encrypt (if any)
+   },
+   key, //from generateKey or importKey above
+   data //ArrayBuffer of the data
+  )
+  .then(function(decrypted) {
+   //returns an ArrayBuffer containing the decrypted data
+   var data = new Uint8Array(decrypted);
+   return data
+  })
+  .catch(function(err) {
+   console.error(err);
+  });
 }
 
-//RSA
-var rsaPublicKey;
-var rsaPrivateKey;
-generateRSA().then((key) => {
-  rsaPublicKey = key.publicKey;
-  rsaPrivateKey = key.privateKey;
+// RSA Keys
+var rsaPublicKey; // My public
+var rsaPrivateKey; // my private
+var rsaPublicKey_2; // Other users public key
 
-  // console.log(rsaPublicKey);
-  // console.log(rsaPrivateKey);
-});
 
 //AES
-var aesKey;
-generateAES().then((res) => {
-  aesKey = res;
-  // console.log(aesKey);
-  var buffer = new ArrayBuffer(100);
-  // console.log(buffer);
-
-  AESEncrypt(aesKey, buffer).then((res) => {
-    aesKey = res[0];
-    var encryptedData = res[1];
-    var generatedIv = res[2];
-    // console.log(encryptedData);
-
-    AESDecrypt(aesKey, encryptedData, generatedIv).then((res) => {
-      var data = res;
-      // console.log(data);
-    });
-  });
-});
+// var aesKey;
+// generateAES().then((res) => {
+//  aesKey = res;
+//  // console.log(aesKey);
+//  var buffer = new ArrayBuffer(100);
+//  // console.log(buffer);
+//
+//  AESEncrypt(aesKey, buffer).then((res) => {
+//   aesKey = res[0];
+//   var encryptedData = res[1];
+//   var generatedIv = res[2];
+//   // console.log(encryptedData);
+//
+//   AESDecrypt(aesKey, encryptedData, generatedIv).then((res) => {
+//    var data = res;
+//    // console.log(data);
+//   });
+//  });
+// });
 
 $(function() {
-  // would like to put protocol here but it cant find socketio. @TODO
+ // would like to put protocol here but it cant find socketio. @TODO
 });
