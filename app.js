bf6b7f77f8706138f0046f596d191c601571e647
James <14151626@studentmail.ul.ie>
2018-02-23 00:26:19 +0000

    initial tests

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
+const SocketIOFile = require('socket.io-file');
 var chatEncrypted = false;
 var userCount = 0;
 
